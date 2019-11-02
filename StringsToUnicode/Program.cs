5e15fc1bded3b6809f1c042b76157a2f046d481a
Carpenteri1 <36822443+Carpenteri1@users.noreply.github.com>
2019-11-02 11:21:25 +0100

    Add project files.

---
+++ StringsToUnicode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsToUnicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hi!";
+            string format = "";
+
+            for (int i = 0; i < input.Length; i++)
+                format += string.Format("\\u{0:X4}", (int)input[i]);
+            
+            Console.Write(format);
+            Console.ReadKey();
+
+        }
+    }
+}
